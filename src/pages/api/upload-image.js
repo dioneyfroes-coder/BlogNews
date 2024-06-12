714032a44106a1b931199808aa3497fecdb71fa5
dioneyfroes-coder <dioney.froes@hotmail.com>
2024-06-12 13:40:15 -0300

    Remoção do axios e substituição pelo fetch nativo

--- src/pages/api/upload-image.js
+++ src/pages/api/upload-image.js
@@ -1,20 +1,21 @@
-import axios from 'axios';
-
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { url } = req.body;
     const apiKey = process.env.IMGBB_API_KEY;
 
     try {
-      const response = await axios.post(`https://api.imgbb.com/1/upload`, null, {
-        params: {
-          key: apiKey,
-          image: url,
-        },
+      const response = await fetch(`https://api.imgbb.com/1/upload?key=${apiKey}&image=${url}`, {
+        method: 'POST',
       });
 
-      if (response.data && response.data.data && response.data.data.url) {
-        return res.status(200).json({ imageUrl: response.data.data.url });
+      if (!response.ok) {
+        throw new Error('Failed to upload image');
+      }
+
+      const data = await response.json();
+
+      if (data && data.data && data.data.url) {
+        return res.status(200).json({ imageUrl: data.data.url });
       } else {
         return res.status(500).json({ error: 'Failed to upload image' });
       }
